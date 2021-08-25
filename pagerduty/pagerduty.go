c8c97e2e4af44b8d62588c3f4e0b93d49c27d8fa
petetanton <peter.tanton@dazn.com>
2021-08-25 08:36:41 +0100

    add some logging

--- pagerduty/pagerduty.go
+++ pagerduty/pagerduty.go
@@ -118,6 +118,7 @@ func (c *Client) do(r *http.Request) (*PagerDutyResponse, error) {
 
 	if response.StatusCode >= http.StatusBadRequest {
 		b, _ := ioutil.ReadAll(response.Body)
+		c.cfg.Logger.Errorf("Error on request to %s %s", r.Method, r.URL.String())
 		return nil, fmt.Errorf("got a %d response from PagerDuty with error: %s", response.StatusCode, string(b))
 	}
 
