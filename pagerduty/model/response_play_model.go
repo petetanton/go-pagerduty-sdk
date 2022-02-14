94cccfc12539e62960b51740a12c1f5486fa2a5c
petetanton <peter.tanton@dazn.com>
2022-02-14 19:57:18 +0000

    add responder type

--- pagerduty/model/response_play_model.go
+++ pagerduty/model/response_play_model.go
@@ -7,10 +7,22 @@ type ResponsePlay struct {
 	Team               *ApiObject   `json:"team,omitempty"`
 	Subscribers        []*ApiObject `json:"subscribers,omitempty"`
 	SubscribersMessage string       `json:"subscribers_message,omitempty"`
-	Responders         []*ApiObject `json:"responders,omitempty"`
+	Responders         []*Responder `json:"responders,omitempty"`
 	RespondersMessage  string       `json:"responders_message,omitempty"`
 	Runnability        string       `json:"runnability"`
 	ConferenceNumber   string       `json:"conference_number,omitempty"`
 	ConferenceUrl      string       `json:"conference_url,omitempty"`
 	ConferenceType     string       `json:"conference_type,omitempty"`
+}
+
+type Responder struct {
+	Type                       string            `json:"type,omitempty"`
+	ID                         string            `json:"id,omitempty"`
+	Name                       string            `json:"name,omitempty"`
+	Description                string            `json:"description,omitempty"`
+	NumLoops                   int               `json:"num_loops,omitempty"`
+	OnCallHandoffNotifications string            `json:"on_call_handoff_notifications,omitempty"`
+	EscalationRules            []*EscalationRule `json:"escalation_rules,omitempty"`
+	Services                   []*ApiObject      `json:"services,omitempty"`
+	Teams                      []*ApiObject      `json:"teams,omitempty"`
 }
